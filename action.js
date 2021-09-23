c46f82cf6035d0d0697a0263f9d826f4098bb24f
Lawrence <lawrence@acode.ninja>
2021-09-23 22:04:57 +0100

    Initial spike into functionality

---
+++ action.js
@@ -0,0 +1,53 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fg = require('fast-glob');
+const {readFile} = require('fs/promises')
+const chalk = require('chalk');
+const fastq = require('fastq');
+
+class FoundSecret extends Error {
+}
+
+(async () => {
+  const secrets = core.getInput('secrets')
+    .split("\n")
+    .map(secret => secret.split(": "));
+
+  const patterns = core.getInput('patterns')
+    .split("\n");
+
+  const entries = await fg(patterns, {dot: true});
+
+  const foundSecrets = [];
+  const errors = [];
+
+  const queue = fastq.promise(async (entry) => {
+    console.log(chalk.blue(`Searching ${entry} for secrets`));
+
+    const file = await readFile(entry, 'utf8');
+
+    secrets.forEach(([name, value]) => {
+      if (file.includes(value)) {
+        foundSecrets.push(`Found ${name} in ${entry}`);
+      }
+    });
+  }, 20);
+
+  entries.forEach(
+    entry => queue.push(entry)
+      .then()
+      .catch(e => errors.push(e))
+  );
+
+  await queue.drained();
+
+  if (foundSecrets.length !== 0) {
+    core.setFailed(`Found some secrets!`);
+    console.error(foundSecrets.join('\n'));
+  }
+})().then(() => {
+  console.log(chalk.green('No secrets were found!'))
+}).catch(error => {
+  console.log(chalk.redBright('Something went really wrong here!'))
+  core.setFailed(error.message);
+})
---
+++ fixtures/script/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+{
+  echo "$TEST_SECRET_1";
+  echo "$TEST_SECRET_2";
+  echo "$TEST_SECRET_3";
+  echo "$TEST_SECRET_4";
+  echo "$TEST_SECRET_5";
+} >> output.txt
